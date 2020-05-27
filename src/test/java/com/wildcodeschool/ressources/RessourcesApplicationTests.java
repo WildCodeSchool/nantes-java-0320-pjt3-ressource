382c3bbf9d2d142aca444c2dfec982f265992f4e
bastienwcs <bastien@wldcodeschool.fr>
2020-05-27 13:53:07 +0200

    init

---
+++ src/test/java/com/wildcodeschool/ressources/RessourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.ressources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
