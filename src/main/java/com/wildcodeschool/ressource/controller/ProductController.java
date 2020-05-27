6adcb8aceb72d15248a21c73d0e053937baf2de0
mroldan747 <mroldan747@gmail.com>
2020-05-27 17:17:09 +0200

    fragment head

---
+++ src/main/java/com/wildcodeschool/ressource/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.wildcodeschool.ressource.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/result")
+    public String result() {
+        return "/result";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "/product";
+    }
+}
