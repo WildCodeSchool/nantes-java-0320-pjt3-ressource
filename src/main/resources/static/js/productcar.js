6e652c0e8077845dfa6b1c9b26b9d22c7310d3dd
elisego <elise.godard@gmail.com>
2020-06-05 18:12:12 +0200

    changement du carousel bot

--- src/main/resources/static/js/productcar.js
+++ src/main/resources/static/js/productcar.js
@@ -1,17 +1,43 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+/* carousel bottom **/
 $(document).ready(function(){
-    $('.your-class').slick({
+    $('.product-carousel-bot').slick({
         dots: false,
-        infinite: true,
+        infinite: false,
         speed: 300,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 4,
         arrows: true,
-        prevArrow:'.arrow_prev',
-        nextArrow:'.arrow_next',
+        prevArrow:'.arrow_prev_like',
+        nextArrow:'.arrow_next_like',
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1500,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
@@ -20,17 +46,57 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 800,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.product-carousel-bottom-review').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: true,
+        prevArrow:'.arrow_prev_rev',
+        nextArrow:'.arrow_next_rev',
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: false,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
                 }
             }
             // You can unslick at a given breakpoint now by adding:
