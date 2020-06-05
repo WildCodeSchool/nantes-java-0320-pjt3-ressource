66760b053c19780ad5027e0d30ebd7a24177a9d7
elisego <elise.godard@gmail.com>
2020-06-05 18:22:42 +0200

    suppression css inutile

--- src/main/resources/static/js/productcar.js
+++ src/main/resources/static/js/productcar.js
@@ -25,7 +25,7 @@ function showSlides(n) {
 }
 
 /* carousel bottom **/
-$(document).ready(function(){
+$(document).ready(function () {
     $('.product-carousel-bot').slick({
         dots: false,
         infinite: false,
@@ -33,8 +33,8 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: true,
-        prevArrow:'.arrow_prev_like',
-        nextArrow:'.arrow_next_like',
+        prevArrow: '.arrow_prev_like',
+        nextArrow: '.arrow_next_like',
         responsive: [
             {
                 breakpoint: 1500,
@@ -71,8 +71,8 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: true,
-        prevArrow:'.arrow_prev_rev',
-        nextArrow:'.arrow_next_rev',
+        prevArrow: '.arrow_prev_rev',
+        nextArrow: '.arrow_next_rev',
         responsive: [
             {
                 breakpoint: 1500,
