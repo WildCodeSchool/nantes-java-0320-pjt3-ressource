1bdec7229414e438b7ba6baea22bd73103924880
mroldan747 <mroldan747@gmail.com>
2020-06-09 17:40:59 +0200

    materials

--- src/main/java/com/wildcodeschool/ressource/controller/ProductController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/ProductController.java
@@ -1,15 +1,38 @@
 package com.wildcodeschool.ressource.controller;
 
+import com.wildcodeschool.ressource.entity.CareLabel;
+import com.wildcodeschool.ressource.entity.Material;
+import com.wildcodeschool.ressource.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
 public class ProductController {
 
+    @Autowired
+    private CareLabelRepository careLabelRepository;
+    @Autowired
+    private CertificationRepository certificationRepository;
+    @Autowired
+    private FeatureRepository featureRepository;
+    @Autowired
+    private ImgProductRepository imgProductRepository;
+    @Autowired
+    private MaterialRepository materialRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+
     @GetMapping("/results")
-    public String result() {
+    public String result(Model model) {
+
+        model.addAttribute("materials", materialRepository.findAll());
         return "results";
     }
 
--- src/main/java/com/wildcodeschool/ressource/entity/Feature.java
+++ src/main/java/com/wildcodeschool/ressource/entity/Feature.java
@@ -3,7 +3,7 @@ package com.wildcodeschool.ressource.entity;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import
+
 
 @Entity
 @Table(name = "feature")
