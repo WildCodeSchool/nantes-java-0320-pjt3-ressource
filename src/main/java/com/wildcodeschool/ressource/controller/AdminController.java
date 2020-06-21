872aded7a53369ef3acc3b263171d1c4e739ea9e
mroldan747 <mroldan747@gmail.com>
2020-06-21 14:31:36 +0200

    nav-bar fragment and admin done

---
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.ressource.controller;
+
+import org.hibernate.tool.schema.internal.exec.GenerationTarget;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin/admin")
+    public String adminAdmin() {
+        return "admin_admin";
+    }
+}
