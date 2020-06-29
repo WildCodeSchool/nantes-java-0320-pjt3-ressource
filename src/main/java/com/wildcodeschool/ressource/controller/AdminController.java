01dc4d4f92b8a3b8ad9c80df65a5bb529d5e8e57
mroldan747 <mroldan747@gmail.com>
2020-06-29 18:16:12 +0200

    login admin html css

--- src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    @GetMapping("/admin")
+    public String adminLogin() {
+        return "admin_login";
+    }
+
     @GetMapping("/admin/admin")
     public String adminAdmin() {
         return "admin_admin";
