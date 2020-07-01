61547df34fdd7acea0e964ea30eedbe7321adc53
mroldan747 <mroldan747@gmail.com>
2020-07-01 11:50:06 +0200

    get the admin from db

--- src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -1,11 +1,27 @@
 package com.wildcodeschool.ressource.controller;
 
+import com.wildcodeschool.ressource.entity.Admin;
+import com.wildcodeschool.ressource.entity.Role;
+import com.wildcodeschool.ressource.repository.AdminRepository;
+import com.wildcodeschool.ressource.repository.RoleRepository;
 import org.hibernate.tool.schema.internal.exec.GenerationTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @GetMapping("/admin")
     public String adminLogin() {
@@ -13,8 +29,18 @@ public class AdminController {
     }
 
     @GetMapping("/admin/admin")
-    public String adminAdmin() {
+    public String adminAdmin(Model model) {
+
+        List<Admin> admins = adminRepository.findAll();
+        List<Role> roles = roleRepository.findAll();
+        model.addAttribute("admins", admins);
+        model.addAttribute("roles", roles);
         return "admin_admin";
+    }
+
+    @PostMapping("/admin/admin/create")
+    public String adminCreate(@ModelAttribute Admin admin) {
+        return "redirect:/admin/admin";
     }
 
     @GetMapping("/admin/companies")
