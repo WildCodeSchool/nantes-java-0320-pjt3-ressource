4a91a050fdbc4d57f7c885090b7507e794799fad
ganout <morgan.bourdillel@gmail.com>
2020-07-01 14:17:22 +0200

    text dynamic admin-companies

--- src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -1,11 +1,21 @@
 package com.wildcodeschool.ressource.controller;
 
+import com.wildcodeschool.ressource.entity.Company;
+import com.wildcodeschool.ressource.repository.CompanyRepository;
 import org.hibernate.tool.schema.internal.exec.GenerationTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @GetMapping("/admin")
     public String adminLogin() {
@@ -18,10 +28,26 @@ public class AdminController {
     }
 
     @GetMapping("/admin/companies")
-    public String adminCompanies() {
+    public String adminCompanies(Model model) {
+
+        List<Company> companyList = companyRepository.findAll();
+        model.addAttribute("companies", companyList);
         return "admin-companies";
     }
-  
+
+    @GetMapping("/admin/company/{name}")
+    public String getCompanySelected(@PathVariable String name, Model model) {
+
+        if (name.equals("new")) {
+            Company company = new Company();
+            model.addAttribute("companySelected", company);
+        } else {
+            Company companySelected = companyRepository.findByName(name);
+            model.addAttribute("companySelected", companySelected);
+        }
+        return "admin-company-selected";
+    }
+
     @GetMapping("/admin/product")
     public String adminProduct() {
         return "productAdmin";
