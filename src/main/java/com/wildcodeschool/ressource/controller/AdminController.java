9ea9da47b3854cbe095c4539e5a1f5497f2cf8ec
ganout <morgan.bourdillel@gmail.com>
2020-07-01 15:56:04 +0200

    post companies completed

--- src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class AdminController {
@@ -42,10 +45,27 @@ public class AdminController {
             Company company = new Company();
             model.addAttribute("companySelected", company);
         } else {
-            Company companySelected = companyRepository.findByName(name);
-            model.addAttribute("companySelected", companySelected);
+            Optional<Company> optionalCompany = companyRepository.findByName(name);
+            if (optionalCompany.isPresent()) {
+                Company companySelected = optionalCompany.get();
+                model.addAttribute("companySelected", companySelected);
+            }
         }
         return "admin-company-selected";
+    }
+
+    @PostMapping("admin/companies")
+    public String postCompany(Company companySelected) {
+
+        Optional<Company> optionalCompany = companyRepository.findByName(companySelected.getName());
+        if (optionalCompany.isPresent()) {
+            Company companyModified = optionalCompany.get();
+            companySelected.setId(companyModified.getId());
+            companyRepository.save(companySelected);
+        } else {
+            companyRepository.save(companySelected);
+        }
+        return "redirect:/admin/companies";
     }
 
     @GetMapping("/admin/product")
