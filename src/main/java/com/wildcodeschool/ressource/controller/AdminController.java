3128a51edc80216f8e387c912abaa46f7d3ff3e1
mroldan747 <mroldan747@gmail.com>
2020-07-16 15:53:57 +0200

    fix of bugs on results and products

--- src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
+++ src/main/java/com/wildcodeschool/ressource/controller/AdminController.java
@@ -205,6 +205,9 @@ public class AdminController {
     @PostMapping("/admin/products/search")
     public String productSearch(Model model, @RequestParam String reference) {
 
+        Admin admin = userService.getLoggedUsername();
+        model.addAttribute("admin", admin);
+
         Product productModified = new Product();
 
         Optional<Product> optionalProduct = productRepository.findByReference(reference);
@@ -250,6 +253,7 @@ public class AdminController {
     @GetMapping("/admin/products/delete")
     public String deleteProduct(@RequestParam Long id){
 
+        imgProductRepository.deleteByProductId(id);
         compositionRepository.deleteByProductId(id);
         productRepository.deleteById(id);
 
--- src/main/java/com/wildcodeschool/ressource/repository/ImgProductRepository.java
+++ src/main/java/com/wildcodeschool/ressource/repository/ImgProductRepository.java
@@ -3,10 +3,21 @@ package com.wildcodeschool.ressource.repository;
 import com.wildcodeschool.ressource.entity.Company;
 import com.wildcodeschool.ressource.entity.ImageProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface ImgProductRepository extends JpaRepository<ImageProduct, Long> {
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM image_product WHERE product_id = :productId",
+            nativeQuery = true)
+    void deleteByProductId(@Param("productId") Long productId);
 }
