a5288a63f6c258e944275bf4461c2a0f618b17f4
mroldan747 <mroldan747@gmail.com>
2020-07-28 16:15:22 +0200

    js works well for all 12 images

--- src/main/resources/static/js/admin-companies.js
+++ src/main/resources/static/js/admin-companies.js
@@ -36,23 +36,24 @@ let companySelected = companies.addEventListener('change', function() {
 
 // 5MB Images not allowed
 let form = document.getElementById("new-company-admin");
-let inputFileLogo = document.getElementById("logo");
-let inputFilePicFromSky = document.getElementById("picFromSky");
-let inputFileCompMap = document.getElementById("compMap");
-let inputFileCeoPhoto = document.getElementById("ceo-photo");
-let inputFileThumbnailOneTopLeft = document.getElementById("thumbnailOneTopLeft");
-let inputFileThumbnailOneTopMiddle = document.getElementById("thumbnailOneTopMiddle");
-let inputFileThumbnailOneTopRight = document.getElementById("thumbnailOneTopRight");
-let inputFileThumbnailSideWords = document.getElementById("thumbnailSideWords");
-let inputFileThumbnailTwoTopLeft = document.getElementById("thumbnailTwoTopLeft");
-let inputFileThumbnailTwoTopMiddle= document.getElementById("thumbnailTwoTopMiddle");
-let inputFileThumbnailTwoTopRight= document.getElementById("thumbnailTwoTopRight");
-let inputFileThumbnailTwoSideWords= document.getElementById("thumbnailTwoSideWords");
-let inputFileEndPhoto = document.getElementById("endPhoto");
 
 
 
 form.addEventListener("submit", function (event) {
+
+    let inputFileLogo = document.getElementById("logo");
+    let inputFilePicFromSky = document.getElementById("picFromSky");
+    let inputFileCompMap = document.getElementById("compMap");
+    let inputFileCeoPhoto = document.getElementById("ceo-photo");
+    let inputFileThumbnailOneTopLeft = document.getElementById("thumbnailOneTopLeft");
+    let inputFileThumbnailOneTopMiddle = document.getElementById("thumbnailOneTopMiddle");
+    let inputFileThumbnailOneTopRight = document.getElementById("thumbnailOneTopRight");
+    let inputFileThumbnailSideWords = document.getElementById("thumbnailSideWords");
+    let inputFileThumbnailTwoTopLeft = document.getElementById("thumbnailTwoTopLeft");
+    let inputFileThumbnailTwoTopMiddle= document.getElementById("thumbnailTwoTopMiddle");
+    let inputFileThumbnailTwoTopRight= document.getElementById("thumbnailTwoTopRight");
+    let inputFileThumbnailTwoSideWords= document.getElementById("thumbnailTwoSideWords");
+    let inputFileEndPhoto = document.getElementById("endPhoto");
 
     let size = 5000000;
     let logo = inputFileLogo.files[0];
@@ -69,68 +70,50 @@ form.addEventListener("submit", function (event) {
     let thumbnailTwoSideWords = inputFileThumbnailTwoSideWords.files[0];
     let endPhoto = inputFileEndPhoto.files[0];
 
+    if ((logo && logo.size > size) || (picFromSky && picFromSky.size > size) || (compMap && compMap.size > size) ||
+        (ceoPhoto && ceoPhoto.size > size) || (thumbnailOneTopLeft && thumbnailOneTopLeft.size > size) ||
+        (thumbnailOneTopMiddle && thumbnailOneTopMiddle.size > size) ||
+        (thumbnailOneTopRight && thumbnailOneTopRight.size > size) || (thumbnailSideWords && thumbnailSideWords.size > size) ||
+        (thumbnailTwoTopLeft && thumbnailTwoTopLeft.size > size) || (thumbnailTwoTopMiddle && thumbnailTwoTopMiddle.size > size) ||
+        (thumbnailTwoTopRight && thumbnailTwoTopRight.size > size) || (thumbnailTwoSideWords && thumbnailTwoSideWords.size > size) ||
+        (endPhoto && endPhoto.size > size)) {
 
-    if (logo.size > size){
         event.preventDefault();
-        errorImage("error-logo", "input-logo");
-        return false;
-    } else if (picFromSky.size > size) {
-        event.preventDefault();
-        errorImage("error-picFromSky", "input-picFromSky");
-        return false;
-    } else if (compMap.size > size) {
-        event.preventDefault();
-        errorImage("error-compMap", "input-compMap");
-        return false;
-    } else if (ceoPhoto.size > size) {
-        event.preventDefault();
-        errorImage("error-ceo-photo", "input-ceo-photo");
-        return false;
-    } else if (thumbnailOneTopLeft.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailOneTopLeft", "input-ThumbnailOneTopLeft");
-        return false;
-    } else if (thumbnailOneTopMiddle.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailOneTopMiddle", "input-ThumbnailOneTopMiddle");
-        return false;
-    } else if (thumbnailOneTopRight.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailOneTopRight", "input-ThumbnailOneTopRight");
-        return false;
-    } else if (thumbnailSideWords.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailSideWords", "input-ThumbnailSideWords");
-        return false;
-    } else if (thumbnailTwoTopLeft.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailTwoTopLeft", "input-ThumbnailTwoTopLeft");
-        return false;
-    } else if (thumbnailTwoTopMiddle.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailTwoTopMiddle", "input-ThumbnailTwoTopMiddle");
-        return false;
-    } else if (thumbnailTwoTopRight.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailTwoTopRight", "input-ThumbnailTwoTopRight");
-        return false;
-    } else if (thumbnailTwoSideWords.size > size) {
-        event.preventDefault();
-        errorImage("error-ThumbnailTwoSideWords", "input-ThumbnailTwoSideWords");
-        return false;
-    } else if (endPhoto.size > size) {
-        event.preventDefault();
-        errorImage("error-EndPhoto", "input-EndPhoto");
+        if (logo && logo.size > size){
+            errorImage("input-logo");
+        } else if (picFromSky && picFromSky.size > size) {
+            errorImage("input-picFromSky");
+        } else if (compMap && compMap.size > size) {
+            errorImage("input-compMap");
+        } else if (ceoPhoto && ceoPhoto.size > size) {
+            errorImage("input-ceo-photo");
+        } else if (thumbnailOneTopLeft && thumbnailOneTopLeft.size > size) {
+            errorImage("input-ThumbnailOneTopLeft");
+        } else if (thumbnailOneTopMiddle && thumbnailOneTopMiddle.size > size) {
+            errorImage("input-ThumbnailOneTopMiddle");
+        } else if (thumbnailOneTopRight && thumbnailOneTopRight.size > size) {
+            errorImage("input-ThumbnailOneTopRight");
+        } else if (thumbnailSideWords && thumbnailSideWords.size > size) {
+            errorImage("input-ThumbnailSideWords");
+        } else if (thumbnailTwoTopLeft && thumbnailTwoTopLeft.size > size) {
+            errorImage("input-ThumbnailTwoTopLeft");
+        } else if (thumbnailTwoTopMiddle && thumbnailTwoTopMiddle.size > size) {
+            errorImage("input-ThumbnailTwoTopMiddle");
+        } else if (thumbnailTwoTopRight && thumbnailTwoTopRight.size > size) {
+            errorImage("input-ThumbnailTwoTopRight");
+        } else if (thumbnailTwoSideWords && thumbnailTwoSideWords.size > size) {
+            errorImage("input-ThumbnailTwoSideWords");
+        } else if (endPhoto && endPhoto.size > size) {
+            errorImage("input-EndPhoto");
+        }
         return false;
     }
     return  true;
 });
 
-function errorImage(id, input) {
-    let span = document.getElementById(id);
+function errorImage(input) {
+
     let inputFile = document.getElementById(input);
     inputFile.classList.remove("valid");
     inputFile.classList.add("invalid");
-    span.innerText = "The size of the image must be less than 5MB";
-    span.style.color = "red";
 }
